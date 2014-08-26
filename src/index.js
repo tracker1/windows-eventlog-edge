94a816530b253c65df4d5e730e62447f7d3f8bfc
Michael J. Ryan <michaelr@classiccars.com>
2014-08-26 08:21:44 -0700

    add utility method for EventLog module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var genBoundWriters = require('./utils/genBoundWriters');
+var writeLog = require('./write-log');
 
-module.exports = {
-  Application: genBoundWriters.bind(null,"Application"),
-  Security: genBoundWriters.bind(null,"Security"),
-  Setup: genBoundWriters.bind(null,"Setup"),
-  System: genBoundWriters.bind(null,"System"),
-  ForwardedEvents: genBoundWriters.bind(null,"Forwarded Events")
-};
+function EventLog(){
+  writeLog.apply(null, arguments);
+}
+
+EventLog.Application = genBoundWriters.bind(null,"Application");
+EventLog.Security = genBoundWriters.bind(null,"Security");
+EventLog.Setup = genBoundWriters.bind(null,"Setup");
+EventLog.System = genBoundWriters.bind(null,"System");
+EventLog.ForwardedEvents = genBoundWriters.bind(null,"Forwarded Events");
+
+module.exports = EventLog;
--- test/windows-eventlog-edge.tests.js
+++ test/windows-eventlog-edge.tests.js
@@ -9,7 +9,7 @@ var expect = chai.expect;
 chai.use(require('sinon-chai'));
 
 describe('windows-eventlog-edge/index', function(){
-  var sut, stubs, genBoundWriters;
+  var sut, stubs, genBoundWriters, writeLog;
 
   beforeEach(function(){
     stubs = {
@@ -20,6 +20,8 @@ describe('windows-eventlog-edge/index', function(){
         ForwardedEvents: {}
     };
 
+    writeLog = sinon.stub();
+
     genBoundWriters = sinon.stub();
     genBoundWriters.withArgs('Application').returns(stubs.Application);
     genBoundWriters.withArgs('Security').returns(stubs.Security);
@@ -28,9 +30,16 @@ describe('windows-eventlog-edge/index', function(){
     genBoundWriters.withArgs('Forwarded Events').returns(stubs.ForwardedEvents);
 
     sut = pr('../src/index', {
+      './write-log': writeLog,
       './utils/genBoundWriters': genBoundWriters
     });
 
+  });
+
+  it('is a functino that will passthrough to write-log', function(){
+    var cb = function(){};
+    sut('test1',cb);
+    expect(writeLog).to.be.calledWithExactly('test1',cb);
   });
 
   it('will call the expected methods', function(){
